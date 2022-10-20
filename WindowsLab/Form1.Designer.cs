2845d8475df8a873b2f12dc7b9b7df41073d2c93
joekw <joekw@RYZEN7>
2022-10-20 14:51:22 -0300

    Agregar archivos de proyecto.

---
+++ WindowsLab/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace WindowsLab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAncho = new System.Windows.Forms.TextBox();
+            this.txtAlto = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtAncho
+            // 
+            this.txtAncho.Location = new System.Drawing.Point(137, 100);
+            this.txtAncho.Name = "txtAncho";
+            this.txtAncho.Size = new System.Drawing.Size(276, 29);
+            this.txtAncho.TabIndex = 0;
+            // 
+            // txtAlto
+            // 
+            this.txtAlto.Location = new System.Drawing.Point(137, 187);
+            this.txtAlto.Name = "txtAlto";
+            this.txtAlto.Size = new System.Drawing.Size(276, 29);
+            this.txtAlto.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(132, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ancho";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(132, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Alto";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(120, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(340, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ingresa los datos para calcular el área";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(206, 236);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(155, 50);
+            this.btnCalcular.TabIndex = 5;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 310);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAlto);
+            this.Controls.Add(this.txtAncho);
+            this.Name = "Form1";
+            this.Text = "Calcular el área de un rectángulo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAncho;
+        private System.Windows.Forms.TextBox txtAlto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ WindowsLab/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsLab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (txtAncho.Text.Contains("."))
+            {
+                txtAncho.Text.Replace(".", ",");
+            }
+            if (txtAlto.Text.Contains("."))
+            {
+                txtAlto.Text.Replace(".", ",");
+            }
+            AreaRectangulo(txtAncho.Text, txtAlto.Text);
+        }
+
+        private void AreaRectangulo(string anchoRect, string altoRect)
+        {
+            if (txtAncho.Text.Contains("."))
+            {
+                txtAncho.Text.Replace(".", ",");
+            }
+            if (txtAlto.Text.Contains("."))
+            {
+                txtAlto.Text.Replace(".", ",");
+            }
+
+            bool baseConvertida = double.TryParse(txtAncho.Text, out double baseRectangulo);
+            bool altoConvertido = double.TryParse(txtAlto.Text, out double altoRectangulo);
+
+            if (baseConvertida && altoConvertido)
+            {
+                double area = baseRectangulo * altoRectangulo;
+                MessageBox.Show("El área del rectángulo es " + area);
+            }
+            else
+            {
+                MessageBox.Show("Hubo algún problema con los datos ingresados, revisa que sean correctos.");
+            }
+        }
+    }
+}
