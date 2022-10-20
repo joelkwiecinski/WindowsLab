171e45e8a53aae13d9bb349350392b9f305e53c1
Joe Kwiecinski <joekwiecinski@outlook.com>
2022-10-20 18:00:42 -0300

    Cambios

--- WindowsLab/Form6.Designer.cs
+++ WindowsLab/Form6.Designer.cs
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.dateFechaNacimiento = new System.Windows.Forms.DateTimePicker();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(164, 61);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(301, 29);
+            this.txtApellido.TabIndex = 2;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(164, 21);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(301, 29);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // dateFechaNacimiento
+            // 
+            this.dateFechaNacimiento.Location = new System.Drawing.Point(75, 112);
+            this.dateFechaNacimiento.Name = "dateFechaNacimiento";
+            this.dateFechaNacimiento.Size = new System.Drawing.Size(390, 29);
+            this.dateFechaNacimiento.TabIndex = 3;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(71, 24);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(87, 25);
+            this.lblNombre.TabIndex = 4;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(70, 64);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(88, 25);
+            this.lblApellido.TabIndex = 5;
+            this.lblApellido.Text = "Apellido:";
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(185, 165);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(164, 60);
+            this.btnVerificar.TabIndex = 6;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // Form6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form6";
+            this.ClientSize = new System.Drawing.Size(534, 249);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.dateFechaNacimiento);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtApellido);
+            this.Name = "Form6";
+            this.Text = "Validación de datos personales";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.DateTimePicker dateFechaNacimiento;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Button btnVerificar;
     }
 }
--- WindowsLab/Form6.cs
+++ WindowsLab/Form6.cs
@@ -16,5 +16,41 @@ namespace WindowsLab
         {
             InitializeComponent();
         }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            if (txtNombre.Text != "" && txtApellido.Text != "")
+            {
+                imprimir(txtNombre.Text, txtApellido.Text, dateFechaNacimiento.Value);
+            }
+            else
+            {
+                MessageBox.Show("Faltan datos.");
+            }
+        }
+
+        private void imprimir(string nombre, string apellido)
+        {
+            MessageBox.Show("Bienvenido, " + nombre + " " + apellido + ".");
+        }
+
+        private void imprimir(string nombre, string apellido, DateTime fechaNacimiento)
+        {
+            int anoNacimiento = fechaNacimiento.Year;
+            int diaNacimiento = fechaNacimiento.DayOfYear;
+
+            if (diaNacimiento < DateTime.Today.DayOfYear)
+            {
+                int edad = DateTime.Today.Year - anoNacimiento;
+                // Ya cumplió años
+                MessageBox.Show(nombre + " " + apellido + " tiene " + edad + " años.");
+            }
+            else
+            {
+                int edad = (DateTime.Today.Year - 1) - anoNacimiento;
+                // Todavía no cumple años
+                MessageBox.Show(nombre + " " + apellido + " tiene " + edad + " años.");
+            }
+        }
     }
 }
