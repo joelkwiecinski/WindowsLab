acbe668b9b91cc2f28943a40402044e5d8c74075
Joe Kwiecinski <joekwiecinski@outlook.com>
2022-10-21 15:31:59 -0300

    Ejercicio 1 de vectores realizado

---
+++ WindowsLab/Form7.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace WindowsLab
+{
+    partial class Form7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerarInforme = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGenerarInforme
+            // 
+            this.btnGenerarInforme.Location = new System.Drawing.Point(150, 66);
+            this.btnGenerarInforme.Name = "btnGenerarInforme";
+            this.btnGenerarInforme.Size = new System.Drawing.Size(448, 54);
+            this.btnGenerarInforme.TabIndex = 2;
+            this.btnGenerarInforme.Text = "Generar informe de notas";
+            this.btnGenerarInforme.UseVisualStyleBackColor = true;
+            this.btnGenerarInforme.Click += new System.EventHandler(this.btnGenerarInforme_Click);
+            // 
+            // Form7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 184);
+            this.Controls.Add(this.btnGenerarInforme);
+            this.Name = "Form7";
+            this.Text = "Form7";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnGenerarInforme;
+    }
+}
---
+++ WindowsLab/Form7.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace WindowsLab
+{
+    public partial class Form7 : Form
+    {
+
+        string[] alumnos;
+        double[,] notasAlumnos;
+        double[] promedios;
+        double[] mayorNota;
+
+        public Form7()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerarInforme_Click(object sender, EventArgs e)
+        {
+            GenerarInforme();
+        }
+
+
+
+        #region metodos
+
+        private void GenerarInforme()
+        {
+            alumnos = new string[4];
+            notasAlumnos = new double[4, 3];
+            promedios = new double[4];
+            mayorNota = new double[4];
+
+            SolicitarAlumnos();
+            SolicitarNotas();
+            GenerarPromedios();
+            InformarCondicion();
+        }
+
+        private void SolicitarAlumnos()
+        {
+            for (int i = 0; i < alumnos.Length; i++)
+            {
+                alumnos[i] = Interaction.InputBox("Ingrese el nombre del alumno " + (i + 1));
+                while (alumnos[i].Length == 0)
+                {
+                    MessageBox.Show("Debe ingresar un nombre");
+                    alumnos[i] = Interaction.InputBox("Ingrese el nombre del alumno " + (i + 1));
+                }
+            }
+        }
+
+        private void SolicitarNotas()
+        {
+            for (int i = 0; i < alumnos.Length; i++)
+            {
+                for (int j = 0; j < notasAlumnos.GetLength(1); j++)
+                {
+                    bool convertido = double.TryParse(Interaction.InputBox("Ingrese la nota de " + alumnos[i] + " en la MATERIA " + (j + 1)), out double notaIngresada);
+                    while (!convertido)
+                    {
+                        MessageBox.Show("Debes ingresar una nota");
+                        convertido = double.TryParse(Interaction.InputBox("Ingrese la nota de " + alumnos[i] + " en la MATERIA " + (j + 1)), out notaIngresada);
+                    }
+                    notasAlumnos[i, j] = notaIngresada;
+                }
+            }
+        }
+
+        private void GenerarPromedios()
+        {
+            for (int i = 0; i < alumnos.Length; i++)
+            {
+                double sumaTotal = 0;
+                for (int j = 0; j < notasAlumnos.GetLength(1); j++)
+                {
+                    sumaTotal += notasAlumnos[i, j];
+                }
+                MessageBox.Show("El promedio de notas de " + alumnos[i] + " es " + (sumaTotal / notasAlumnos.GetLength(1)));
+            }
+        }
+
+        private void InformarCondicion()
+        {
+            for (int i = 0; i < alumnos.Length; i++)
+            {
+                string mensajeParaMostrar = "";
+                for (int j = 0; j < notasAlumnos.GetLength(1); j++)
+                {
+                    if (mensajeParaMostrar == "")
+                    {
+                        mensajeParaMostrar = alumnos[i] + " en la MATERIA " + (j + 1) + " sacó un " + notasAlumnos[i, j];
+                    }
+                    else
+                    {
+                        mensajeParaMostrar += "\n" + alumnos[i] + " en la MATERIA " + (j + 1) + " sacó un " + notasAlumnos[i, j];
+                    }
+
+                    if (notasAlumnos[i, j] < 4)
+                    {
+                        mensajeParaMostrar += ", debe ir a recuperatorio.";
+                    }
+                    else if (notasAlumnos[i, j] > 4 && notasAlumnos[i, j] <= 7)
+                    {
+                        mensajeParaMostrar += ", bien.";
+                    }
+                    else if (notasAlumnos[i, j] > 7)
+                    {
+                        mensajeParaMostrar += ", muy bien.";
+                    }
+                }
+                MessageBox.Show(mensajeParaMostrar);
+            }
+        }
+
+        #endregion
+    }
+}
